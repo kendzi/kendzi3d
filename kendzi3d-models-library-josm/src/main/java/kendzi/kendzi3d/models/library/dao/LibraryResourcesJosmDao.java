28e304b0c48359785a7803d23dedac01e49b78f6
kendzi <kendzi@gmail.com>
2013-10-11 23:57:22 +0200

    Model library storage inside JOSM properties.

---
+++ kendzi3d-models-library-josm/src/main/java/kendzi/kendzi3d/models/library/dao/LibraryResourcesJosmDao.java
@@ -0,0 +1,63 @@
+/*
+ * This software is provided "AS IS" without a warranty of any kind.
+ * You use it on your own risk and responsibility!!!
+ *
+ * This file is shared under BSD v3 license.
+ * See readme.txt and BSD3 file for details.
+ *
+ */
+package kendzi.kendzi3d.models.library.dao;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.openstreetmap.josm.Main;
+
+/**
+ * Load and store resources path in memory.
+ * 
+ * @author Tomasz Kędziora (Kendzi)
+ */
+public class LibraryResourcesJosmDao implements LibraryResourcesDao {
+
+    /**
+     * Property key for list of resources used by models library.
+     */
+    private static final String KENDZI_3D_MODELS_LIBRARY_RESOURCES_URLS = "kendzi3d.models_library.resources_urls";
+
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see kendzi.kendzi3d.models.library.dao.LibraryResourcesDao#loadResourcesPath()
+     */
+    @Override
+    public List<String> loadResourcesPath() {
+        List<String> paths = new ArrayList<String>(Main.pref.getCollection(KENDZI_3D_MODELS_LIBRARY_RESOURCES_URLS));
+        if (paths == null | paths.isEmpty()) {
+            return defaultResources();
+        }
+        return paths;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see kendzi.kendzi3d.models.library.dao.LibraryResourcesDao#saveResourcesPath(java.util.List)
+     */
+    @Override
+    public void saveResourcesPath(List<String> fileKeys) {
+        Main.pref.putCollection(KENDZI_3D_MODELS_LIBRARY_RESOURCES_URLS, fileKeys);
+    }
+
+    @Override
+    public void setDefaultResourcesPaths() {
+        Main.pref.put(KENDZI_3D_MODELS_LIBRARY_RESOURCES_URLS, null);
+    }
+
+    private static List<String> defaultResources() {
+        return new ArrayList<String>(Arrays.asList("plugin:/models/modelsLibraryInternalLayer.xml", "plugin:/models/modelsLibraryLayer.xml",
+                "plugin:/models/trafficSignsLibraryInternalLayer.xml"));
+    }
+}
