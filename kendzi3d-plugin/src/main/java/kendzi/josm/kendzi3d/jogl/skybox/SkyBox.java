b14220da218627acbaabe2c3e0294913b3cf09dc
kendzi <kendzi@gmail.com>
2014-02-23 19:12:03 +0100

    format

--- kendzi3d-plugin/src/main/java/kendzi/josm/kendzi3d/jogl/skybox/SkyBox.java
+++ kendzi3d-plugin/src/main/java/kendzi/josm/kendzi3d/jogl/skybox/SkyBox.java
@@ -1,6 +1,8 @@
 package kendzi.josm.kendzi3d.jogl.skybox;
 
+import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
+import javax.media.opengl.fixedfunc.GLLightingFunc;
 import javax.vecmath.Point3d;
 
 import kendzi.jogl.camera.Camera;
@@ -24,15 +26,15 @@ public class SkyBox {
 
     }
 
-    public void draw(GL2 gl , Camera camera, Perspective perspective3d) {
+    public void draw(GL2 gl, Camera camera, Perspective perspective3d) {
 
-        gl.glDisable(GL2.GL_DEPTH_TEST);
-        gl.glColor4f((float) 255/255, (float)255/255, (float)255/255, 1f);
+        gl.glDisable(GL.GL_DEPTH_TEST);
+        gl.glColor4f((float) 255 / 255, (float) 255 / 255, (float) 255 / 255, 1f);
 
-        gl.glDisable(GL2.GL_LIGHTING);
-        gl.glEnable(GL2.GL_TEXTURE_2D);
+        gl.glDisable(GLLightingFunc.GL_LIGHTING);
+        gl.glEnable(GL.GL_TEXTURE_2D);
 
-        //XXX
+        // XXX
         String frontTextureName = "/textures/free/sky1/sfront37.jpg";
         String rightTextureName = "/textures/free/sky1/sright37.jpg";
         String leftTextureName = "/textures/free/sky1/sleft37.jpg";
@@ -44,42 +46,26 @@ public class SkyBox {
 
         gl.glTranslated(p.x, p.y, p.z);
 
-        drawPolygon(gl, frontTextureName,
-                new Point3d(-10, -10, -10),
-                new Point3d(10, -10, -10),
-                new Point3d(10, 10, -10),
+        drawPolygon(gl, frontTextureName, new Point3d(-10, -10, -10), new Point3d(10, -10, -10), new Point3d(10, 10, -10),
                 new Point3d(-10, 10, -10));
-        drawPolygon(gl, backTextureName,
-                new Point3d(10, -10, 10),
-                new Point3d(-10, -10, 10),
-                new Point3d(-10, 10, 10),
+        drawPolygon(gl, backTextureName, new Point3d(10, -10, 10), new Point3d(-10, -10, 10), new Point3d(-10, 10, 10),
                 new Point3d(10, 10, 10));
-//
-        drawPolygon(gl, rightTextureName,
-                new Point3d(10, -10, -10),
-                new Point3d(10, -10,  10),
-                new Point3d(10, 10,  10),
+        //
+        drawPolygon(gl, rightTextureName, new Point3d(10, -10, -10), new Point3d(10, -10, 10), new Point3d(10, 10, 10),
                 new Point3d(10, 10, -10));
-//
-        drawPolygon(gl, leftTextureName,
-                new Point3d(-10, -10, 10),
-                new Point3d(-10, -10, -10),
-                new Point3d(-10, 10, -10),
+        //
+        drawPolygon(gl, leftTextureName, new Point3d(-10, -10, 10), new Point3d(-10, -10, -10), new Point3d(-10, 10, -10),
                 new Point3d(-10, 10, 10));
 
-        drawPolygon(gl, topTextureName,
-                new Point3d(-10, 10, -10),
-                new Point3d(10, 10, -10),
-                new Point3d(10, 10, 10),
+        drawPolygon(gl, topTextureName, new Point3d(-10, 10, -10), new Point3d(10, 10, -10), new Point3d(10, 10, 10),
                 new Point3d(-10, 10, 10));
 
         gl.glPopMatrix();
-        gl.glDisable(GL2.GL_TEXTURE_2D);
+        gl.glDisable(GL.GL_TEXTURE_2D);
 
+        gl.glColor4f((float) 255 / 255, (float) 255 / 255, (float) 255 / 255, (float) 255 / 255);
 
-        gl.glColor4f((float) 255/255, (float)255/255, (float)255/255, (float) 255/255);
-
-        gl.glEnable(GL2.GL_DEPTH_TEST);
+        gl.glEnable(GL.GL_DEPTH_TEST);
 
     }
 
@@ -93,7 +79,7 @@ public class SkyBox {
      */
     public void drawPolygon(GL2 gl, String frontTextureName, Point3d p1, Point3d p2, Point3d p3, Point3d p4) {
         TextureCoords tc = new TextureCoords(0, 0, 1, 1);
-        if (frontTextureName!= null) {
+        if (frontTextureName != null) {
             Texture texture = this.textureCacheService.getTexture(gl, frontTextureName);
 
             texture.enable(gl);
@@ -105,25 +91,24 @@ public class SkyBox {
         gl.glBegin(GL2.GL_POLYGON);
 
         gl.glTexCoord2d(tc.left(), tc.bottom());
-        gl.glVertex3d(p1.x ,p1.y, p1.z);
+        gl.glVertex3d(p1.x, p1.y, p1.z);
         gl.glTexCoord2d(tc.right(), tc.bottom());
-        gl.glVertex3d(p2.x ,p2.y, p2.z);
+        gl.glVertex3d(p2.x, p2.y, p2.z);
         gl.glTexCoord2d(tc.right(), tc.top());
-        gl.glVertex3d(p3.x ,p3.y, p3.z);
-        gl.glTexCoord2d(tc.left() , tc.top());
-        gl.glVertex3d(p4.x ,p4.y, p4.z);
+        gl.glVertex3d(p3.x, p3.y, p3.z);
+        gl.glTexCoord2d(tc.left(), tc.top());
+        gl.glVertex3d(p4.x, p4.y, p4.z);
 
         gl.glEnd();
 
+        // gl.glColor3f((float) 0/255, (float)0/255, (float)255/255);
 
-//        gl.glColor3f((float) 0/255, (float)0/255, (float)255/255);
+        // gl.glPushMatrix();
+        // gl.glTranslated(x, 0.1, z);
+        // DrawUtil.drawDotY(gl, 0.5, 12);
+        // gl.glPopMatrix();
 
-//        gl.glPushMatrix();
-//        gl.glTranslated(x, 0.1, z);
-//        DrawUtil.drawDotY(gl, 0.5, 12);
-//        gl.glPopMatrix();
-
-        if (frontTextureName!= null) {
+        if (frontTextureName != null) {
             Texture t = this.textureCacheService.getTexture(gl, frontTextureName);
             if (t != null) {
                 t.disable(gl);
