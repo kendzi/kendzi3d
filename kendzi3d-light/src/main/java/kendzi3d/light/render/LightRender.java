97cf97a424da8d44e3a4da227b2e6d01ddcd85d8
Taylor Smock <tsmock@fb.com>
2022-02-23 14:39:57 -0700

    light: Convert to lwjgl (internals)
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- kendzi3d-light/src/main/java/kendzi3d/light/render/LightRender.java
+++ kendzi3d-light/src/main/java/kendzi3d/light/render/LightRender.java
@@ -1,13 +1,12 @@
 package kendzi3d.light.render;
 
 import com.jogamp.opengl.GL2;
-import com.jogamp.opengl.fixedfunc.GLLightingFunc;
-import com.jogamp.opengl.fixedfunc.GLMatrixFunc;
 
 import javax.inject.Inject;
 
 import kendzi.jogl.Gl2Draw;
 import kendzi3d.light.service.LightRenderService;
+import org.lwjgl.opengl.GL11;
 
 public class LightRender implements Gl2Draw {
 
@@ -23,33 +22,33 @@ public class LightRender implements Gl2Draw {
     @Override
     public void draw(GL2 gl) {
 
-        // gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
+        // GL11.glMatrixMode(GL11.GL_MODELVIEW);
         //
         // // enable a single light source
-        // gl.glEnable(GLLightingFunc.GL_LIGHTING);
-        // gl.glEnable(GLLightingFunc.GL_LIGHT0);
+        // GL11.glEnable(GL11.GL_LIGHTING);
+        // GL11.glEnable(GL11.GL_LIGHT0);
 
-        gl.glLightfv(GLLightingFunc.GL_LIGHT0, GLLightingFunc.GL_AMBIENT, lightRenderService.getAmbientLightColor(), 0);
+        GL11.glLightfv(GL11.GL_LIGHT0, GL11.GL_AMBIENT, lightRenderService.getAmbientLightColor());
 
         float[] diffuseLightColor = lightRenderService.getDiffuseLightColor();
         // bright white diffuse
-        gl.glLightfv(GLLightingFunc.GL_LIGHT0, GLLightingFunc.GL_DIFFUSE, diffuseLightColor, 0);
+        GL11.glLightfv(GL11.GL_LIGHT0, GL11.GL_DIFFUSE, diffuseLightColor);
         // and specular
-        gl.glLightfv(GLLightingFunc.GL_LIGHT0, GLLightingFunc.GL_SPECULAR, diffuseLightColor, 0);
+        GL11.glLightfv(GL11.GL_LIGHT0, GL11.GL_SPECULAR, diffuseLightColor);
 
         // float lightPos[] = { 1.0f, 1.0f, 1.0f, 0.0f }; // top right front
 
         // direction_
-        gl.glLightfv(GLLightingFunc.GL_LIGHT0, GLLightingFunc.GL_POSITION, lightRenderService.getLightPosition(), 0);
+        GL11.glLightfv(GL11.GL_LIGHT0, GL11.GL_POSITION, lightRenderService.getLightPosition());
     }
 
     public void init(GL2 gl) {
 
-        gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
         // enable a single light source
-        gl.glEnable(GLLightingFunc.GL_LIGHTING);
-        gl.glEnable(GLLightingFunc.GL_LIGHT0);
+        GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_LIGHT0);
     }
 
 }
