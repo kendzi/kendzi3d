c875e2fd36083a72f21788fbff4f2917c2ddf0b3
Taylor Smock <tsmock@fb.com>
2022-03-09 14:00:52 -0700

    Viewport: Fix the occasional NPE on initial viewport load
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- kendzi3d-jogl-util/src/main/java/kendzi/jogl/camera/Viewport.java
+++ kendzi3d-jogl-util/src/main/java/kendzi/jogl/camera/Viewport.java
@@ -62,7 +62,7 @@ public class Viewport implements ViewportPicker {
     /**
      * Look direction.
      */
-    private transient Vector3d view;
+    private transient Vector3d view = new Vector3d();
 
     private transient Vector3d screenHorizontally = new Vector3d();
 
